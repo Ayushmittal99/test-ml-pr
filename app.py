f65a68bf998f9a378ca543521e7340682944220e
mittalayush520@gmail.com <mittalayush520@gmail.com>
2026-02-04 12:32:28 +0530

    added

---
+++ app.py
@@ -0,0 +1,5 @@
+import numpy as numpy
+import pandas as pd 
+
+df = pd.read_csv("C:\\Users\\pc\\test_ml_pr\\covid_toy - covid_toy.csv")
+print(df.head(2))
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from test-ml-pr!")
+
+
+if __name__ == "__main__":
+    main()
